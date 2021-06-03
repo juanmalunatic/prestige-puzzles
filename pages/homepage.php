2fa2a61ee72713877f878b990ddaab627b33cd0d
Juan Manuel Luna López <lunalopezjm@gmail.com>
2021-06-03 02:05:59 -0500

    Homepage polish 1st section and implement 2nd

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,58 +1,101 @@
-<div class="container">
-    <div class="row">
-        <div class="col-xs-12 col-md-7">
-            <div class="text-size-largest text-style-bold">
-                <div>
-                    Finished you <span class="text-purple-dark">jigsaw puzzle</span>?
+<div class="white-hero-spacer"></div>
+<div class="section section-white pb-5">
+    <div class="container pb-5">
+        <div class="row">
+            <div class="col-xs-12 col-md-7 col-lg-6">
+                <div class="text-size-largest text-style-bold pt-4">
+                    <div>
+                        Finished you <span class="text-purple-dark">jigsaw puzzle</span>?
+                    </div>
+                    <div>
+                        Enjoy it forever.
+                    </div>
                 </div>
-                <div>
-                    Enjoy it forever.
-                </div>
-            </div>
 
-            <div class="mt-5">
-                We transform your precious puzzle into magnificent + memorable wall art.
-            </div>
+                <div class="mt-5">
+                    We transform your precious puzzle into magnificent + memorable wall art.
+                </div>
 
-            <div class="container-fluid mt-5 mb-5 text-size-small">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-6">
-                        <a href="/shop/puzzle" class="but but-purple full-width">
-                            Plaque my puzzle
-                        </a>
+                <div class="container-fluid mt-5 mb-5 text-size-small">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6">
+                            <a href="/shop/puzzle" class="but but-purple full-width">
+                                Plaque my puzzle
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 mt4-xs align-items-center-flex">
+                            <a href="/how-to-ship-to-us">
+                                See plaquing process
+                            </a>
+                        </div>
                     </div>
-                    <div class="col-xs-12 col-sm-6 mt4-xs align-items-center-flex">
-                        <a href="/how-to-ship-to-us">
-                            See plaquing process
-                        </a>
+                </div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-12 mt4-sm col-lg-4 align-items-center-flex text-align-left">
+                            <div class="home-simage home-simage-lock"></div>
+                            <div class="home-simage-text text-bold-smallcaps">
+                                Secure Shipping
+                            </div>
+                        </div>
+                        <div class="col-xs-12 mt4-sm col-lg-4 align-items-center-flex text-align-left">
+                            <div class="home-simage home-simage-heart"></div>
+                            <div class="home-simage-text text-bold-smallcaps">
+                                Careful Handling
+                            </div>
+                        </div>
+                        <div class="col-xs-12 mt4-sm col-lg-4 align-items-center-flex text-align-left">
+                            <div class="home-simage home-simage-gift"></div>
+                            <div class="home-simage-text text-bold-smallcaps">
+                                Safe Return
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xs-12 mt4-sm col-lg-4 align-items-center-flex text-align-left">
-                        <div class="home-simage home-simage-lock"></div>
-                        <div class="home-simage-text text-bold-smallcaps">
-                            Secure Shipping
-                        </div>
-                    </div>
-                    <div class="col-xs-12 mt4-sm col-lg-4 align-items-center-flex text-align-left">
-                        <div class="home-simage home-simage-heart"></div>
-                        <div class="home-simage-text text-bold-smallcaps">
-                            Careful Handling
-                        </div>
-                    </div>
-                    <div class="col-xs-12 mt4-sm col-lg-4 align-items-center-flex text-align-left">
-                        <div class="home-simage home-simage-gift"></div>
-                        <div class="home-simage-text text-bold-smallcaps">
-                            Safe Return
-                        </div>
-                    </div>
-                </div>
+            <div class="col-xs-12 col-md-5 col-lg-6 pl-5 pr-2">
+                <div id="home-framed-puzzle" class="ml-3"></div>
             </div>
         </div>
-        <div class="col-xs-12 col-md-5">
-            <div id="home-framed-puzzle"></div>
+    </div>
+</div>
+<div class="section section-gray pt-5 pb-5">
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-7">
+                <h3 class="text-center mb-5 pb-3 text-bold">
+                    Our team of skilled puzzlers still 'oooh!' over each puzzle plaque creation
+                </h3>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-md stats-highlight mb-4">
+                <span class="stats-highlight-number text-green">20</span>
+                <span class="text-bold-smallcaps">Years experience</span>
+                <span class="text-size-smaller">creating puzzle plaque magic</span>
+            </div>
+            <div class="col-xs-6 col-md stats-highlight mb-4">
+                <span class="stats-highlight-number text-purple">10357</span>
+                <span class="text-bold-smallcaps">Puzzles laminated</span>
+                <span class="text-size-smaller">and mounted onto a wood plaque</span>
+            </div>
+            <div class="col-xs-6 col-md stats-highlight mb-4">
+                <span class="stats-highlight-number text-yellow">7M+</span>
+                <span class="text-bold-smallcaps">Puzzle pieces</span>
+                <span class="text-size-smaller">heated in our mounting machinery</span>
+            </div>
+            <div class="col-xs-6 col-md stats-highlight mb-4">
+                <span class="stats-highlight-number text-blue-light">42'' x 66''</span>
+                <span class="text-bold-smallcaps">Largest puzzle</span>
+                <span class="text-size-smaller">mounted at our workshop</span>
+            </div>
+            <div class="col-xs-6 col-md stats-highlight mb-4">
+                <span class="stats-highlight-number text-red">0%</span>
+                <span class="text-bold-smallcaps">Puzzles dismantled</span>
+                <span class="text-size-smaller">during the process</span>
+            </div>
         </div>
     </div>
 </div>
