8caa4ae961f51f7507c2a3807c103e9079f6645a
Juan Manuel Luna López <lunalopezjm@gmail.com>
2021-07-29 11:40:35 -0500

    Implement reference into homepage. CSS fixes needed

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,4 +1,9 @@
+<?php
+$tpurl = get_template_directory_uri();
+?>
+
 <div class="white-hero-spacer"></div>
+
 <div class="section section-white pb-5">
     <div class="container pb-5">
         <div class="row">
@@ -59,6 +64,7 @@
         </div>
     </div>
 </div>
+
 <div class="section section-gray pt-5 pb-5">
     <div class="container pt-5 pb-5">
         <div class="row">
@@ -99,3 +105,203 @@
         </div>
     </div>
 </div>
+
+<section class="puzzle-numbers">
+    <h2>
+        You spent endless recreational hours piecing your puzzle, it would be a shame to…
+    </h2>
+    <ul>
+        <li><span class="icon-one">1</span>stash it out of sight, under the mattress.</li>
+        <li><span class="icon-two">2</span>break it apart, back into its original box.</li>
+        <li><span class="icon-three">3</span>haphazardly glue it onto a cardboard backing.</li>
+    </ul>
+</section>
+
+<section class="add-pizzazz">
+    <h2>Add pizzazz to your puzzle...</h2>
+    <div class="container p-0">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="add-pizzazz-box">
+                    <img src="<?=$tpurl?>/img/Rectangle11.png" alt="pizzazz">
+                    <h5>Enhanced quality</h5>
+                    <p>
+                        A <b>water-resistant, matte sheen</b> gives your puzzle extra luster + make it easy to
+                        clean.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="add-pizzazz-box">
+                    <img src="<?=$tpurl?>/img/Rectangle12.png" alt="pizzazz">
+                    <h5>Custom color finish</h5>
+                    <p>
+                        Pretty <b>frameless edges in 29 color choices</b> to match your puzzle or room decor.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="add-pizzazz-box">
+                    <img src="<?=$tpurl?>/img/Rectangle13.png" alt="pizzazz">
+                    <h5>Strong backing</h5>
+                    <p>
+                        High-quality <b>MDF wood mount</b> backing makes it durable and long-lasting.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="sensation">
+    <h2>Turn your puzzle creation into an art sensation</h2>
+    <div class="container p-0">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="sensation-content">
+                    <img src="<?=$tpurl?>/img/Rectangle-11.png" alt="sensation">
+                    <h5>CLASSIC DECOR</h5>
+                    <p>
+                        Decorate your home, office, or vacation villa with an up-to-date puzzle wall art
+                        —prominently showcasing your <b>handiwork</b>.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="sensation-content">
+                    <img src="<?=$tpurl?>/img/Rectangle-12.png" alt="sensation">
+                    <h5>TREASURED MEMOIR</h5>
+                    <p>
+                        Remember the <b>quality time</b> spent on your own, or with loved ones. Searching, finding,
+                        assembling and finally seeing it come together.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="sensation-content">
+                    <img src="<?=$tpurl?>/img/Rectangle-13.png" alt="sensation">
+                    <h5>GIFT TO ADORE</h5>
+                    <p>
+                        Present a deserving recipient with a unique gift that clearly portrays <b>the time, love and
+                            dedication</b> imbued on their behalf.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <a href="#" class="btn-purple">Frame my puzzle</a>
+    </div>
+</section>
+
+<section class="workshop">
+    <h2>Here’s how you can be sure your puzzle gets to our workshop in one piece.</h2>
+    <div class="workshop-row">
+        <div class="workshop-box">
+            <span class="icon-glue">1</span>
+            <img src="<?=$tpurl?>/img/glue.png" alt="workshop">
+        </div>
+        <div class="workshop-box arrow">
+            <span class="icon-pack">2</span>
+            <img src="<?=$tpurl?>/img/pack.png" alt="workshop">
+        </div>
+        <div class="workshop-box">
+            <span class="icon-ship">3</span>
+            <img src="<?=$tpurl?>/img/ship.png" alt="workshop">
+        </div>
+    </div>
+    <a href="#">Step-by-Step shipping directions </a>
+</section>
+
+<section class="slide-content">
+    <div class="slide-content-container">
+        <div class="swiper-container swiper-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="slide-text">
+                        <p>
+                            “Every time I plaque a puzzle, it challenges me to purchase a larger size puzzle. The
+                            beautiful uplift each puzzle receives at Preserve Your Puzzle makes the sometimes
+                            frustrating puzzle-time worthwhile!”
+                        </p>
+                        <div class="name">
+                            <h5>Brenda Aisenstock</h5>
+                            <p>Brooklyn</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="slide-text">
+                        <p>
+                            “Every time I plaque a puzzle, it challenges me to purchase a larger size puzzle. The
+                            beautiful uplift each puzzle receives at Preserve Your Puzzle makes the sometimes
+                            frustrating puzzle-time worthwhile!”
+                        </p>
+                        <div class="name">
+                            <h5>Brenda Aisenstock - 2</h5>
+                            <p>Brooklyn - 2</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-navigation">
+            <div class="swiper-button-next"><img src="<?=$tpurl?>/img/arrow-round-forward.svg" alt="arrow"></div>
+            <div class="swiper-button-prev"><img src="<?=$tpurl?>/img/arrow-round-forward.svg" alt="arrow"></div>
+        </div>
+    </div>
+</section>
+
+<section class="puzzle-facts">
+    <div class="puzzle-facts-swiper swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="puzzle-slide-text">
+                    <img src="<?=$tpurl?>/img/tl-g.svg" alt="puzzle">
+                    <h2>Puzzle facts</h2>
+                    <p>
+                        In the early stages of dementia or Alzheimer’s, puzzles are said to <b>ease some of the
+                            symptoms</b> and provides stimulating comfort and sense of control to the patients. Puzzles,
+                        especially those on display, can reawaken memories in patients and <b>improve mental speed</b>.
+                    </p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="puzzle-slide-text">
+                    <img src="<?=$tpurl?>/img/tl-g.svg" alt="puzzle">
+                    <h2>Puzzle facts</h2>
+                    <p>
+                        In the early stages of dementia or Alzheimer’s, puzzles are said to <b>ease some of the
+                            symptoms</b> and provides stimulating comfort and sense of control to the patients. Puzzles,
+                        especially those on display, can reawaken memories in patients and <b>improve mental speed</b>.
+                    </p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="puzzle-slide-text">
+                    <img src="<?=$tpurl?>/img/tl-g.svg" alt="puzzle">
+                    <h2>Puzzle facts</h2>
+                    <p>
+                        In the early stages of dementia or Alzheimer’s, puzzles are said to <b>ease some of the
+                            symptoms</b> and provides stimulating comfort and sense of control to the patients. Puzzles,
+                        especially those on display, can reawaken memories in patients and <b>improve mental speed</b>.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
+</section>
+
+<section class="art-gallery">
+    <div class="art-gallery-info">
+        <h2>In 14 days, your puzzle will be an art gallery masterpiece!</h2>
+        <p>
+            You buy a puzzle...we make your purchase worthwhile. <br>
+            You put in hard work...we ensure secure shipping both ways. <br>
+            You invest time... we create a timeless piece!
+        </p>
+        <div class="frame-puzzle-button">
+            <a href="#" class="btn-purple">Frame my puzzle</a>
+            <a href="#">See finishing options</a>
+        </div>
+    </div>
+</section>
